129d8f3b6c2155e8dec283387a139db4c66aadf4
naoki-Shimada <45224370+naoki-Shimada@users.noreply.github.com>
2026-01-29 16:38:42 +0900

    Add files via upload

--- products.php
+++ products.php
@@ -2,14 +2,15 @@
 <?php require 'includes/header.php'; ?>
 
     <div class="BreadList">
-        <div class="BreadListBorder">
         <p>TOP>商品一覧</p>
-        </div>
     </div>
+    <div class="BreadListBorder">
+    </div>
+
     <div class="BreadList">
-        <div class="BreadListBorder">
         <p>ようこそ、<span class="UserName">様。</p>
-        </div>
+    </div>
+    <div class="BreadListBorder">
     </div>
 
     <section class="ProductSection">
@@ -18,43 +19,55 @@
         <h3 class="ProductSubTitle">メインメニュー</h3>
             <div class="ProductContainer">
                 <div class="ProductItem">
+                    <a href="detail.php?id=1">
                     <img src="images/Original.png" alt="Original">
                     <h3 class="ItemName">CCドーナツ 当店オリジナル（5個入り）</h3>
+                    </a>
                     <p class="ItemPrice">税込 ¥1,500</p>
                     <button class="CartButton">カートに入れる</button>
                 </div>
 
                 <div class="ProductItem">
+                    <a href="detail.php?id=2">
                     <img src="images/Chocolate.png" alt="Chocolate">
                     <h3 class="ItemName">チョコレートデライト（5個入り）</h3>
+                    </a>
                     <p class="ItemPrice">税込 ¥1,600</p>
                     <button class="CartButton">カートに入れる</button>
                 </div>
 
                 <div class="ProductItem">
+                    <a href="detail.php?id=3">
                     <img src="images/CaramelCream.png" alt="CaramelCream">
                     <h3 class="ItemName">キャラメルクリーム（5個入り）</h3>
+                    </a>
                     <p class="ItemPrice">税込 ¥1,600</p>
                     <button class="CartButton">カートに入れる</button>
                 </div>
 
                 <div class="ProductItem">
+                    <a href="detail.php?id=4">
                     <img src="images/Classic.png" alt="Classic">
                     <h3 class="ItemName">プレーンクラシック（5個入り）</h3>
+                    </a>
                     <p class="ItemPrice">税込 ¥1,500</p>
                     <button class="CartButton">カートに入れる</button>
                 </div>
 
                 <div class="ProductItem">
+                    <a href="detail.php?id=5">
                     <img src="images/SummerCitras.png" alt="SummerCitras">
                     <h3 class="ItemName">【新作】サマーシトラス（5個入り）</h3>
+                    </a>
                     <p class="ItemPrice">税込 ¥1,600</p>
                     <button class="CartButton">カートに入れる</button>
                 </div>
 
                 <div class="ProductItem">
+                    <a href="detail.php?id=6">
                     <img src="images/Strawberry.png" alt="Strawberry">
                     <h3 class="ItemName">ストロベリークラッシュ（5個入り）</h3>
+                    </a>
                     <p class="ItemPrice">税込 ¥1,800</p>
                     <button class="CartButton">カートに入れる</button>
                 </div>
@@ -63,43 +76,55 @@
             <h3 class="ProductSubTitle">バラエティセット</h3>
                 <div class="ProductContainer">
                     <div class="ProductItem">
+                        <a href="detail.php?id=7">
                         <img src="images/Fruit_12.png" alt="Fruit_12">
                         <h3 class="ItemName">フルーツドーナツセット（12個入り）</h3>
+                        </a>
                         <p class="ItemPrice">税込 ¥3,500</p>
                         <button class="CartButton">カートに入れる</button>
                     </div>
 
                     <div class="ProductItem">
+                        <a href="detail.php?id=8">
                         <img src="images/Fruit_14.png" alt="Fruit_14">
                         <h3 class="ItemName">フルーツドーナツセット（14個入り）</h3>
+                        </a>
                         <p class="ItemPrice">税込 ¥4,000</p>
                         <button class="CartButton">カートに入れる</button>
                     </div>
 
                     <div class="ProductItem">
+                        <a href="detail.php?id=9">
                         <img src="images/Selection.png" alt="Selection">
                         <h3 class="ItemName">ベストセレクションボックス（4個入り）</h3>
+                        </a>
                         <p class="ItemPrice">税込 ¥1,200</p>
                         <button class="CartButton">カートに入れる</button>
                     </div>
 
                     <div class="ProductItem">
+                        <a href="detail.php?id=10">
                         <img src="images/CrashBox.png" alt="CrashBox">
                         <h3 class="ItemName">クラッシュボックス（7個入り）</h3>
+                        </a>
                         <p class="ItemPrice">税込 ¥2,400</p>
                         <button class="CartButton">カートに入れる</button>
                     </div>
 
                     <div class="ProductItem">
+                        <a href="detail.php?id=11">
                         <img src="images/Cream_4.png" alt="Cream_4">
                         <h3 class="ItemName">クリームボックス（4個入り）</h3>
+                        </a>
                         <p class="ItemPrice">税込 ¥1,400</p>
                         <button class="CartButton">カートに入れる</button>
                     </div>
 
                     <div class="ProductItem">
+                        <a href="detail.php?id=12">
                         <img src="images/Cream_9.png" alt="Cream_9">
                         <h3 class="ItemName">クリームボックス（9個入り）</h3>
+                        </a>
                         <p class="ItemPrice">税込 ¥2,800</p>
                         <button class="CartButton">カートに入れる</button>
                     </div>
